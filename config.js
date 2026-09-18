e3ee0f095059d689bf5bb10f2b35ba1808510f8a
rahim025 <rahimbatchabi7@gmail.com>
2026-09-18 12:17:55 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Adresse du backend AfriPay.
+// En local (serveur + navigateur sur le même ordinateur) : http://localhost:5000
+// Une fois le backend déployé (Render, etc.) : remplace par ton URL, ex :
+// window.AFRIPAY_API_BASE = 'https://afripay-api.onrender.com';
+
+window.AFRIPAY_API_BASE = 'http://localhost:5000';
