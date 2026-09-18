8d5d8552c842efd836ccda45d778f281216352bf
rahim025 <rahimbatchabi7@gmail.com>
2026-09-18 12:48:52 +0100

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// Adresse du backend AfriPay.
+// En local (serveur + navigateur sur le même ordinateur) : http://localhost:5000
+// Backend déployé sur Render :
+
+window.AFRIPAY_API_BASE = 'https://afripay0.onrender.com';
